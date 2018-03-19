a2e68e05443d2aba079adc8ed36ebf0cf86aefbb
Saeed Alalwan <alalwan@gmail.com>
2018-03-18 20:50:28 -0400

    Add stop loss support.

--- place-buy-order.js
+++ place-buy-order.js
@@ -7,51 +7,46 @@ module.exports = async (
     bid_price,
     symbol,
     placeSellOrder = false,
-    placestopLoss = false,
+    placeStopLoss = false,
     customPrice = false,
     sellPrice,
     stopLossPrice
   }
 ) => {
-  // {
-  //     url:"https://api.robinhood.com/instruments/450dfc6d-5510-4d40-abfb-f633b7d9be3e/",
-  //     symbol: "AAPL"
-  //   }
-
-  //   type: "limit",
-  //     quantity: 1,
-  //     bid_price: 150,
-  //     instrument: {
-  //       url:
-  //         "https://api.robinhood.com/instruments/450dfc6d-5510-4d40-abfb-f633b7d9be3e/",
-  //       symbol: "AAPL"
-  //     }
   try {
     let quote;
 
     if (!customPrice) quote = await Robinhood.quote_data(symbol);
-    
+
     let options = {
       type: "limit",
       quantity,
       bid_price: customPrice ? bid_price : quote.results[0].last_trade_price,
       instrument: { url: instrument, symbol }
     };
-    
 
     const orderPlacedRes = await Robinhood.place_buy_order(options);
 
-    if (placeSellOrder) {
-      console.log("sell " + sellPrice);
+    if (placeSellOrder || placeStopLoss) {
       excutedOrder = setInterval(async () => {
         let order = await Robinhood.url(orderPlacedRes.url);
         if (order.state === "filled") {
           console.log("order filled");
-          console.log("placing sell order");
-          await Robinhood.place_sell_order({
-            ...options,
-            bid_price: sellPrice
-          });
+          console.log("placing sell and stop orders");
+          
+          if (placeSellOrder)
+            await Robinhood.place_sell_order({
+              ...options,
+              bid_price: sellPrice
+            });
+
+          if (placeStopLoss)
+            await Robinhood.place_sell_order({
+              quantity,
+              stop_price: stopLossPrice,
+              type: "market",
+              trigger: 'stop'
+            });
 
           clearInterval(excutedOrder);
         }
